04dfbe94f927faa338c1d262d98cf6ee1991f466
mark <rens2588@gmail.com>
2021-02-24 10:01:59 +0200

    invoice

--- app/settings.py
+++ app/settings.py
@@ -109,9 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/static/'
+STATIC_ROOT = '/static/'
 
-STATIC_ROOT = '/var/www/pdf_generator/static/'
+# STATIC_ROOT = '/var/www/pdf_generator/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
